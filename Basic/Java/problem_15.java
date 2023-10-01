b23cf4a455180a937d1e9ea09d0a71ef1b6d3961
Bhavya Kishor Gatya <47660275+BhavyaKishor@users.noreply.github.com>
2023-10-01 23:45:55 +0530

    Update problem_15.java
    1) Concise if statement
    2) Create separate function to add in other classes
    3) Close the Scanner to avoid resource leaks

--- Basic/Java/problem_15.java
+++ Basic/Java/problem_15.java
@@ -1,25 +1,23 @@
-
 import java.util.Scanner;
 
-public class problem_15{
-    public static void main(String args[]) {
+public class problem_15 {
 
-Scanner input = new Scanner (System.in);
-    
-    System.out.print("Provide the Number, Please ");
-    int number = input.nextInt();
-    
-    if( (number == 0 ) || (number%2 == 0) ){
-        
-        System.out.print(number+ " is a Even Number");
-        
-    }else{
-        
-        System.out.print(number +" is a Odd Number");
-        
+    // Function to check if a number is even or odd
+    public static void checkEvenOdd(int number) {
+        String result = (number == 0 || number % 2 == 0) ? "Even" : "Odd";
+        System.out.println(number + " is an " + result + " Number");
     }
 
- 
-   }
-  
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Provide the Number, Please: ");
+        int number = input.nextInt();
+
+        // Call the function to check if the number is even or odd
+        checkEvenOdd(number);
+
+        // Close the Scanner to avoid resource leaks
+        input.close();
+    }
 }
